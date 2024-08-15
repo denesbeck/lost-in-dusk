2bed1a1917d4f17f87a6a269c4af85030ac9e3f4
Denes Beck <denes.beck@hu.ibm.com>
2024-08-15 10:02:01 +0200

    refactor: tailwind animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideInFromBottom: {
+          "0%": {
+            transform: "translateY(2rem)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "translateY(0rem)",
+            opacity: "1",
+          },
+        },
+        textFocus: {
+          "0%": {
+            filter: "blur(12px)",
+            opacity: "0",
+          },
+          "100%": {
+            filter: "blur(0px)",
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        slideInFromBottom: "slideInFromBottom 0.2s ease-out",
+        textFocus: "textFocus 0.5s ease-in",
+      },
+    },
   },
   plugins: [],
 };

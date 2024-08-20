f523e1b09b3db163bc07b85ff4679ec2b5b12477
Denes Beck <denes.beck1@outlook.com>
2024-08-20 14:34:24 +0400

    feat: avatar component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,16 @@
+import { useMemo } from "react";
+import { createAvatar } from "@dicebear/core";
+import { lorelei } from "@dicebear/collection";
+
+function Avatar() {
+  const avatar = useMemo(() => {
+    return createAvatar(lorelei, {
+      size: 128,
+      // ... other options
+    }).toDataUri();
+  }, []);
+
+  return <img src={avatar} alt="Avatar" />;
+}
+
+export default Avatar;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
+import Avatar from "./Avatar";
 import BlogEntry from "./BlogEntry";
 import BlogEntryCard from "./BlogEntryCard";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Navitem from "./Navitem";
 
-export { BlogEntry, BlogEntryCard, Footer, Navbar, Navitem };
+export { Avatar, BlogEntry, BlogEntryCard, Footer, Navbar, Navitem };

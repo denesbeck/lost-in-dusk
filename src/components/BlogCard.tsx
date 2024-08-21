827206301d7a4ccc2b56cc4ad0d598d9c70255d9
Denes Beck <denes.beck1@outlook.com>
2024-08-21 09:14:15 +0400

    fix: types node

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -2,13 +2,16 @@ import { BlogEntry } from "@/interfaces/blog";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const BlogCard = ({ id, title, description, date }: BlogEntry) => (
-  <div className="group relative w-[20rem] max-w-[75vw] animate-textFocus cursor-pointer space-y-2 rounded-md px-6 py-4 ring-2 ring-teal-500 ring-offset-gray-900 transition-all duration-200 ease-in-out hover:ring-offset-4">
+  <button
+    onClick={() => console.log(id)}
+    className="group relative w-[20rem] max-w-[75vw] animate-textFocus cursor-pointer space-y-2 rounded-md px-6 py-4 ring-2 ring-teal-500 ring-offset-gray-900 transition-all duration-200 ease-in-out hover:ring-offset-4"
+  >
     <div className="absolute -top-3.5 left-4 bg-slate-900 px-2 text-base transition-all duration-200 ease-in-out group-hover:-top-4 group-hover:font-bold">
       {title}
     </div>
     <p className="text-sm">{description}</p>
     <p className="text-sm">{date}</p>
-  </div>
+  </button>
 );
 
 export default BlogCard;

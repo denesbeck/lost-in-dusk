76344997609951f1eb8126a4e3375b861dd75688
Denes Beck <denes.beck1@outlook.com>
2024-08-22 21:30:35 +0400

    refactor: avatar specification

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,16 +1,27 @@
 import { useMemo } from "react";
 import { createAvatar } from "@dicebear/core";
-import { lorelei } from "@dicebear/collection";
+import { bottts } from "@dicebear/collection";
 
 function Avatar() {
   const avatar = useMemo(() => {
-    return createAvatar(lorelei, {
-      size: 128,
-      // ... other options
+    return createAvatar(bottts, {
+      size: 90,
+      baseColor: ["546e7a"],
+      eyes: ["frame2"],
+      face: ["round01"],
+      mouth: ["diagram"],
+      sides: ["antenna02"],
+      top: ["bulb01"],
     }).toDataUri();
   }, []);
 
-  return <img src={avatar} alt="Avatar" />;
+  return (
+    <img
+      className="h-[90px] w-[90px] rounded-full p-2 ring-slate-700 transition-all duration-200 ease-in hover:bg-slate-800 hover:ring-2"
+      src={avatar}
+      alt="Avatar"
+    />
+  );
 }
 
 export default Avatar;

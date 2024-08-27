6883e9fadd1c52dee9f8ff3feb5a8014fe4f27ba
Denes Beck <denes.beck@hu.ibm.com>
2024-08-27 14:10:41 +0200

    refactor: blog entry

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,16 +1,20 @@
 import { BlogEntry } from "@/interfaces/blog";
+import { Like } from "@/components";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const BlogCard = ({ id, title, description, date }: BlogEntry) => (
   <button
     onClick={() => console.log(id)}
-    className="group relative w-[20rem] max-w-[75vw] animate-textFocus cursor-pointer space-y-2 rounded-md px-6 py-4 ring-2 ring-teal-500 ring-offset-gray-900 transition-all duration-200 ease-in-out hover:ring-offset-4 active:ring-slate-200"
+    className="flex relative flex-col py-3 px-6 space-y-2 rounded-md ring-2 ring-teal-500 ring-offset-gray-900 transition-all duration-200 ease-in-out cursor-pointer hover:ring-offset-4 group w-[20rem] min-w-[16rem] max-w-[75vw] animate-textFocus active:ring-slate-200"
   >
-    <div className="absolute -top-3.5 left-4 bg-slate-900 px-2 text-base transition-all duration-200 ease-in-out group-hover:-top-4 group-hover:font-bold">
+    <div className="-top-3.5 left-4 text-base text-left transition-all duration-200 ease-in-out sm:absolute sm:px-2 group-hover:-top-4 group-hover:font-bold dark:bg-slate-900">
       {title}
     </div>
-    <p className="text-sm">{description}</p>
-    <p className="text-sm">{date}</p>
+
+    <p className="flex-1 text-sm text-left">{description}</p>
+    <div className="flex justify-between items-center w-full">
+      <Like id={id} />
+      <div className="text-sm">{date}</div>
+    </div>
   </button>
 );
 

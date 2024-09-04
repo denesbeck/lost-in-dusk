4fb1df7f51004d120360f732b0a80fd6e57008c9
Denes Beck <denes.beck1@outlook.com>
2024-09-04 20:39:57 +0200

    feat: contact btn

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,12 +1,17 @@
 import { FaArrowCircleRight } from "react-icons/fa";
 
-const ContactMe = () => {
+interface ContactMeProps {
+  action: () => void;
+}
+
+const ContactMe = ({ action }: ContactMeProps) => {
   return (
     <button
+      onClick={action}
       className={`mt-6 flex animate-textFocus items-center px-3 py-2 text-teal-400 ring-2 ring-teal-400 transition-colors duration-200 ease-in-out hover:bg-teal-400 hover:text-slate-800`}
     >
       Contact me
-      <FaArrowCircleRight className="inline ml-2 text-xl" />
+      <FaArrowCircleRight className="ml-2 inline text-xl" />
     </button>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,10 @@ import { ContactMe } from "@/components";
 import { useTypewriter } from "@/hooks";
 import { Stack } from "@chakra-ui/react";
 
-const Home = () => {
+interface HomeProps {
+  contactAction: () => void;
+}
+const Home = ({ contactAction }: HomeProps) => {
   const output = useTypewriter(
     // The first two spaces are required to prevent the second character from being omitted.
     "  Hello, my name is Denes.|🚀 I'm a DevOps Engineer.",
@@ -11,11 +14,11 @@ const Home = () => {
   return (
     // The height of the div is calculated by subtracting the height of the Navbar, Footer, and the two 5rem paddings from the viewport height.
     <Stack className="h-[calc(100vh-5.25rem-5rem-5rem)] min-h-screen w-full items-center justify-center px-4">
-      <div className="text-2xl text-center min-h-8">{output.split("|")[0]}</div>
-      <div className="text-base text-center min-h-6">
+      <div className="min-h-8 text-center text-2xl">{output.split("|")[0]}</div>
+      <div className="min-h-6 text-center text-base">
         {output.split("|")[1]}
       </div>
-      <ContactMe />
+      <ContactMe action={contactAction} />
     </Stack>
   );
 };

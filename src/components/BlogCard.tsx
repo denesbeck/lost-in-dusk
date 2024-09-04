53dea886a729ec7e2485a24db530b24a2d94d4fe
Denes Beck <denes.beck1@outlook.com>
2024-09-04 20:40:44 +0200

    refactor: design

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -10,14 +10,14 @@ const BlogCard = ({ id, title, description, date }: BlogEntryInterface) => {
     <>
       <button
         onClick={() => setIsVisible(true)}
-        className="flex relative flex-col py-3 px-6 space-y-2 h-auto rounded-md ring-2 ring-teal-500 ring-offset-gray-900 transition-all duration-200 ease-in-out cursor-pointer hover:ring-offset-4 group w-[20rem] min-w-[16rem] max-w-[75vw] animate-textFocus active:ring-slate-200"
+        className="group relative flex h-auto w-[20rem] min-w-[16rem] max-w-[75vw] animate-textFocus cursor-pointer flex-col space-y-2 rounded-md px-6 py-3 ring-2 ring-teal-400 ring-offset-gray-900 transition-all duration-200 ease-in-out hover:ring-offset-4 active:ring-slate-200"
       >
-        <div className="-top-3.5 left-4 text-base text-left transition-all duration-200 ease-in-out sm:absolute sm:px-2 group-hover:-top-4 group-hover:font-bold dark:bg-slate-900">
+        <div className="-top-3.5 left-4 text-left text-base transition-all duration-200 ease-in-out group-hover:-top-4 group-hover:font-bold sm:absolute sm:px-2 dark:bg-slate-900">
           {title}
         </div>
 
-        <p className="flex-1 text-sm text-left">{description}</p>
-        <div className="flex justify-between items-center w-full">
+        <p className="flex-1 text-left text-sm">{description}</p>
+        <div className="flex w-full items-center justify-between">
           <Like id={id} />
           <div className="text-sm">{date}</div>
         </div>

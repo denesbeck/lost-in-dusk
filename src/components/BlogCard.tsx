c0711271d477524bceec5f83e42e0867d6e33bc4
Denes Beck <denes.beck@hu.ibm.com>
2024-09-04 15:32:31 +0200

    refactor: blog card

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,21 +1,34 @@
-import { BlogEntry } from "@/interfaces/blog";
-import { Like } from "@/components";
+import { BlogEntry as BlogEntryInterface } from "@/interfaces/blog";
+import { BlogEntry, Like } from "@/components";
+import { useState } from "react";
+import { BlogEntry01 } from "@/blog-entries";
 
-const BlogCard = ({ id, title, description, date }: BlogEntry) => (
-  <button
-    onClick={() => console.log(id)}
-    className="flex relative flex-col py-3 px-6 space-y-2 rounded-md ring-2 ring-teal-500 ring-offset-gray-900 transition-all duration-200 ease-in-out cursor-pointer hover:ring-offset-4 group w-[20rem] min-w-[16rem] max-w-[75vw] animate-textFocus active:ring-slate-200"
-  >
-    <div className="-top-3.5 left-4 text-base text-left transition-all duration-200 ease-in-out sm:absolute sm:px-2 group-hover:-top-4 group-hover:font-bold dark:bg-slate-900">
-      {title}
-    </div>
+const BlogCard = ({ id, title, description, date }: BlogEntryInterface) => {
+  const [isVisible, setIsVisible] = useState(false);
 
-    <p className="flex-1 text-sm text-left">{description}</p>
-    <div className="flex justify-between items-center w-full">
-      <Like id={id} />
-      <div className="text-sm">{date}</div>
-    </div>
-  </button>
-);
+  return (
+    <>
+      <button
+        onClick={() => setIsVisible(true)}
+        className="flex relative flex-col py-3 px-6 space-y-2 h-auto rounded-md ring-2 ring-teal-500 ring-offset-gray-900 transition-all duration-200 ease-in-out cursor-pointer hover:ring-offset-4 group w-[20rem] min-w-[16rem] max-w-[75vw] animate-textFocus active:ring-slate-200"
+      >
+        <div className="-top-3.5 left-4 text-base text-left transition-all duration-200 ease-in-out sm:absolute sm:px-2 group-hover:-top-4 group-hover:font-bold dark:bg-slate-900">
+          {title}
+        </div>
+
+        <p className="flex-1 text-sm text-left">{description}</p>
+        <div className="flex justify-between items-center w-full">
+          <Like id={id} />
+          <div className="text-sm">{date}</div>
+        </div>
+      </button>
+      {isVisible && (
+        <BlogEntry close={() => setIsVisible(false)}>
+          <BlogEntry01 />
+        </BlogEntry>
+      )}
+    </>
+  );
+};
 
 export default BlogCard;
--- src/components/BlogEntry.tsx
+++ src/components/BlogEntry.tsx
@@ -1,5 +1,24 @@
-const BlogEntry = () => {
-  return <div>BlogEntry</div>;
+import { Stack } from "@chakra-ui/react";
+import { DarkLayout, MacOSBar } from "@/components";
+import { ReactElement } from "react";
+
+interface BlogEntryProps {
+  children: ReactElement;
+  close: () => void;
+}
+
+const BlogEntry = ({ children, close }: BlogEntryProps) => {
+  return (
+    <DarkLayout>
+      <Stack
+        w={"max-content"}
+        className="px-4 pb-8 font-mono rounded-md ring-2 ring-teal-500 w-[60rem] max-w-[90vw] animate-slideInFromBottom dark:text-slate-200"
+      >
+        <MacOSBar close={close} />
+        <div className="overflow-auto px-2 max-h-[80vh]">{children}</div>
+      </Stack>
+    </DarkLayout>
+  );
 };
 
 export default BlogEntry;

8a3f2ae457ff95b9de9138ab3241b845c9cc9cb0
Denes Beck <denes.beck1@outlook.com>
2024-09-04 20:40:35 +0200

    feat: scroll to top

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,8 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect, useRef } from "react";
+
 const ScrollToTop = () => {
+  const navigate = useNavigate();
+  const ref = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!ref.current) return;
+      if (window.scrollY > 200) {
+        ref.current.classList.remove("hidden");
+      } else {
+        ref.current.classList.add("hidden");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <button
-      className="fixed right-8 bottom-8 p-2 text-white bg-gray-800 rounded-md opacity-70 animate-textFocus"
-      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      ref={ref}
+      className="fixed bottom-8 right-8 animate-textFocus rounded-md bg-gray-800 p-2 text-white"
+      onClick={() => navigate("/")}
     >
       Scroll to top
     </button>
--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,17 @@ import App from "./App";
 import "./index.css";
 import { Stack } from "@chakra-ui/react";
 import { ScrollToTop } from "@/components";
+import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/">
-      <Stack className="font-mono dark:text-gray-200 dark:bg-gray-900">
-        <App />
-        <ScrollToTop />
-      </Stack>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter basename="/">
+        <Stack className="font-mono dark:bg-gray-900 dark:text-gray-200">
+          <App />
+          <ScrollToTop />
+        </Stack>
+      </BrowserRouter>
+    </ChakraProvider>
   </StrictMode>,
 );

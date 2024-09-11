f50acd49fa76d6c5e9574ddc183b44a2e8041d44
Denes Beck <denes.beck@hu.ibm.com>
2024-09-11 15:50:24 +0200

    feat: validate input util

---
+++ src/utils/validate.ts
@@ -0,0 +1,35 @@
+const validate = (name: string, email: string, message: string) => {
+  const output: {
+    valid: boolean;
+    messages: string[];
+    invalidFields: string[];
+  } = {
+    valid: false,
+    messages: [],
+    invalidFields: [],
+  };
+
+  if (!name) {
+    output.messages.push("Name is required.");
+    output.invalidFields.push("name");
+  }
+
+  if (!email) {
+    output.messages.push("Email is required.");
+    output.invalidFields.push("email");
+  }
+
+  if (!message) {
+    output.messages.push("Message is required.");
+    output.invalidFields.push("message");
+  }
+
+  if (email && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
+    output.messages.push("Invalid email format.");
+
+  if (output.messages.length === 0) output.valid = true;
+
+  return output;
+};
+
+export default validate;

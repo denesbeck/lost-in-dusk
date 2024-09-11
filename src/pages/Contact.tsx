2957b2227ab163405b000cc8a05f3bd9d271d93d
Denes Beck <denes.beck@hu.ibm.com>
2024-09-11 16:11:26 +0200

    feat: contact me

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,117 @@
 import { Button, Heading2 } from "@/components";
 import { Center, Input, Stack, Textarea } from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
 import { useToast } from "@chakra-ui/react";
+import axios from "axios";
+import validate from "@/utils/validate";
+
+const API_GW_ENDPOINT = import.meta.env.VITE_API_GW_ENDPOINT;
+const SITE_KEY = import.meta.env.VITE_SITE_KEY;
 
 const Contact = () => {
+  const [loading, setLoading] = useState(false);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
+
   const toast = useToast();
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `https://www.google.com/recaptcha/api.js?render=${SITE_KEY}`;
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const handleSubmit = async () => {
+    const { valid, messages } = validate(
+      nameRef.current?.value || "",
+      emailRef.current?.value || "",
+      messageRef.current?.value || "",
+    );
+    if (!valid) {
+      if (!toast.isActive("invalid-input"))
+        toast({
+          id: "invalid-input",
+          title: "Invalid input",
+          description: messages.join("\n"),
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      return;
+    }
+    if (typeof window !== "undefined") {
+      setLoading(true);
+      // @ts-expect-error: grecaptcha is not defined
+      window.grecaptcha.ready(function () {
+        // @ts-expect-error: grecaptcha is not defined
+        window.grecaptcha
+          .execute(SITE_KEY, {
+            action: "submit",
+          })
+          .then(function (token: string) {
+            axios
+              .post(`${API_GW_ENDPOINT}/contactMe`, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  token,
+                  name: nameRef.current?.value,
+                  email: emailRef.current?.value,
+                  message: messageRef.current?.value,
+                }),
+              })
+              .then((res) => {
+                if (res.data.statusCode !== 200)
+                  throw new Error("Unable to send message.");
+                toast({
+                  title: "Success",
+                  description: "Message sent successfully.",
+                  status: "success",
+                  duration: 5000,
+                  isClosable: true,
+                });
+              })
+              .catch(() => {
+                toast({
+                  title: "Error",
+                  description: "Unable to send message.",
+                  status: "error",
+                  duration: 5000,
+                  isClosable: true,
+                });
+                console.error("Unable to send message.");
+                return;
+              })
+              .finally(() => {
+                setLoading(false);
+                nameRef.current!.value = "";
+                emailRef.current!.value = "";
+                messageRef.current!.value = "";
+              });
+          })
+          .catch(() => {
+            toast({
+              title: "Error",
+              description: "Unable to get reCAPTCHA token.",
+              status: "error",
+              duration: 5000,
+              isClosable: true,
+            });
+            console.error("Unable to get reCAPTCHA token.");
+            setLoading(false);
+            return;
+          });
+      });
+    }
+  };
+
   return (
     <Center h={"100vh"}>
       <Stack
@@ -20,6 +128,7 @@ const Contact = () => {
           border={"none"}
           _focusVisible={{ boxShadow: "not-implemented" }}
           className="py-3 px-4 bg-transparent ring-2 ring-teal-400 focus-visible:ring-blue-400 active:ring-blue-400"
+          ref={nameRef}
         />
         <Input
           w={"20rem"}
@@ -28,6 +137,7 @@ const Contact = () => {
           border={"none"}
           _focusVisible={{ boxShadow: "not-implemented" }}
           className="py-3 px-4 bg-transparent ring-2 ring-teal-400 focus-visible:ring-blue-400 active:ring-blue-400"
+          ref={emailRef}
         />
         <Textarea
           w={"20rem"}
@@ -36,23 +146,9 @@ const Contact = () => {
           border={"none"}
           _focusVisible={{ boxShadow: "not-implemented" }}
           className="py-3 px-4 bg-transparent ring-2 ring-teal-400 focus-visible:ring-blue-400 active:ring-blue-400"
+          ref={messageRef}
         />
-        <Button
-          label={"Submit"}
-          action={() => {
-            if (!toast.isActive("not-implemented"))
-              toast({
-                id: "not-implemented",
-                title: "Info",
-                position: "top-right",
-                variant: "subtle",
-                description: "Not implemented yet",
-                status: "info",
-                duration: 5000,
-                isClosable: true,
-              });
-          }}
-        />
+        <Button label={"Submit"} action={handleSubmit} loading={loading} />
       </Stack>
     </Center>
   );

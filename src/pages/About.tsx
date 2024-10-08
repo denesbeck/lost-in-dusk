140c6227328a26ee3ce39b2c1cf492a1fe1c1516
Denes Beck <denes.beck1@outlook.com>
2024-10-08 18:25:21 +0200

    style: about positioning

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,11 +3,11 @@ import { Stack } from "@chakra-ui/react";
 
 const About = () => {
   return (
-    <Stack h={"100vh"} className="flex items-center">
+    <Stack h={"100vh"} className="flex items-center lg:justify-center">
       {/* wrapper for adding animate-slideInFromBottom */}
       <Stack
         w={"max-content"}
-        className="mt-[80px] max-h-[calc(100vh-170px)] max-w-[90vw] animate-slideInFromBottom overflow-auto px-2 py-8 ring-2 ring-teal-400 backdrop-blur-md transition-all duration-200 ease-in-out lg:px-8 lg:hover:shadow-[10px_10px_#1f2937,20px_20px_black]"
+        className="mt-[80px] max-h-[calc(100vh-170px)] max-w-[90vw] animate-slideInFromBottom overflow-auto px-2 py-8 ring-2 ring-teal-400 backdrop-blur-md transition-all duration-200 ease-in-out lg:mt-0 lg:px-8 lg:hover:shadow-[10px_10px_#1f2937,20px_20px_black]"
       >
         {/* main container */}
         <div className="grid grid-cols-1 gap-12 px-4 lg:grid-cols-2">

a75df4b48d6e2216d8791312afc844a7b8dec716
Denes Beck <denes.beck1@outlook.com>
2024-12-31 09:13:25 +0100

    refactor: bio text adj

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -2,7 +2,7 @@ import { Stack } from "@chakra-ui/react";
 import { Heading3 } from "@/components";
 
 const BIO =
-  "Passionate about leveraging technology to streamline software development processes and optimize infrastructure for enhanced efficiency and scalability. Skilled in CI/CD, IaC, automation, container and cloud technologies.";
+  "Passionate about leveraging cutting-edge technology to streamline software development workflows and optimize infrastructure for greater efficiency, scalability, and reliability. As a full-stack developer, I specialize in building modern, high-performance web applications using Next.js, React, Node.js, TypeScript, and AWS. Experienced in implementing CI/CD pipelines, Infrastructure as Code (IaC), and automation to drive seamless development and deployment processes. Focused on delivering high-impact solutions that enhance both developer experience and operational performance.";
 
 const Bio = () => {
   return (
@@ -14,7 +14,7 @@ const Bio = () => {
       px={2}
     >
       <Heading3>Bio</Heading3>
-      <p className="break-words">{BIO}</p>
+      <p className="overflow-y-auto max-h-44 break-words">{BIO}</p>
     </Stack>
   );
 };

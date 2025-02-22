a06d086c0faa7daae11989ca268add47ffff4a81
Denes Beck <denes.beck1@outlook.com>
2025-02-22 19:03:00 +0100

    refactor: avatar

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,23 +1,22 @@
 import { Stack } from "@chakra-ui/react";
 import { Connections, Heading3 } from "@/components";
 import { INFO } from "@/config/data/about";
-import profile from "@/assets/profile.jpeg";
 
 const Info = () => {
   return (
     <Stack maxW={"30rem"} overflowX={"auto"} gap={2} px={2}>
       <Heading3>Info</Heading3>
-      <div className="flex gap-10 items-center pb-3 animate-textFocus">
+      <div className="flex gap-10 items-start pb-3 animate-textFocus">
         <img
-          src={profile}
+          src="https://api.dicebear.com/9.x/bottts-neutral/svg?seed=Aidan"
           alt="profile"
-          className="w-32 h-32 rounded-full ring-2 transition-all duration-200 ease-in hover:scale-110 ring-slate-200 hover:brightness-110"
+          className="w-20 h-20 rounded transition-all duration-200 ease-in hover:scale-110 hover:brightness-110"
         />
         <div>
           {Object.entries(INFO).map(([key, value]) => (
             <div key={key} className="flex space-x-4 max-w-[40vw]">
               <h4 className="text-base font-semibold min-w-[6rem]">{key}</h4>
-              <div >{value}</div>
+              <div>{value}</div>
             </div>
           ))}
           <Connections />

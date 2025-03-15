892dc085925b380d7a1bb97b4b40580d06a68c1d
denesbeck <denesbeck@denesbeck-MacBook-Pro.local>
2025-03-15 21:37:39 +0100

    style: spacing

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -17,7 +17,7 @@ const BlogCard = ({ title, description, date, content }: BlogCardProps) => {
         onClick={() => setIsVisible(true)}
         className="flex flex-col py-3 px-6 space-y-2 h-auto ring-2 ring-teal-400 ring-offset-gray-900 transition-all duration-200 ease-in-out cursor-pointer hover:ring-offset-4 group min-h-[9rem] w-[21rem] min-w-[21rem] max-w-[75vw] animate-textFocus backdrop-blur-md active:ring-slate-200"
       >
-        <h1 className="relative text-lg text-left transition-all duration-200 ease-in-out after:absolute after:-bottom-0.5 after:left-0 after:h-0.5 after:w-0 after:bg-teal-400 after:transition-all after:duration-200 after:ease-in-out after:content-[''] group-hover:font-bold group-hover:after:w-full">
+        <h1 className="relative text-lg text-left whitespace-nowrap transition-all duration-200 ease-in-out after:absolute after:-bottom-0.5 after:left-0 after:h-0.5 after:w-0 after:bg-teal-400 after:transition-all after:duration-200 after:ease-in-out after:content-[''] group-hover:font-bold group-hover:after:w-full">
           {title}
         </h1>
 
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,13 +4,15 @@ const About = () => {
   return (
     <div className="flex flex-col items-center h-screen lg:justify-center">
       {/* wrapper for adding animate-slideInFromBottom */}
-      <div className="grid overflow-auto py-8 px-2 w-max ring-2 ring-teal-400 transition-all duration-200 ease-in-out lg:px-8 lg:mt-0 mt-[80px] max-h-[calc(100vh-170px)] max-w-[90vw] animate-slideInFromBottom backdrop-blur-md lg:hover:shadow-[10px_10px_#1f2937,20px_20px_black]">
-        {/* main container */}
-        <div className="grid grid-cols-1 gap-8 px-4 lg:grid-cols-2">
-          <Info />
-          <Certificates />
-          <Bio />
-          <Skills />
+      <div className="grid py-4 px-2 w-max ring-2 ring-teal-400 transition-all duration-200 ease-in-out lg:py-8 lg:px-8 lg:mt-0 mt-[80px] max-h-[calc(100vh-170px)] max-w-[90vw] animate-slideInFromBottom backdrop-blur-md lg:hover:shadow-[10px_10px_#1f2937,20px_20px_black]">
+        <div className="overflow-auto w-full h-full">
+          {/* main container */}
+          <div className="grid grid-cols-1 gap-8 px-4 lg:grid-cols-2">
+            <Info />
+            <Certificates />
+            <Bio />
+            <Skills />
+          </div>
         </div>
       </div>
     </div>

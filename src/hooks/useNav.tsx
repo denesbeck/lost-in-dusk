25df2f5818d6ec876fd5b1c3c2751f45e3b16712
denesbeck <denesbeck@denesbeck-MacBook-Pro.local>
2025-03-16 07:33:21 +0100

    fix: correct page position when resizing

--- src/hooks/useNav.tsx
+++ src/hooks/useNav.tsx
@@ -1,13 +1,69 @@
-import { useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const useNav = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   const home = useRef<HTMLDivElement | null>(null);
   const blog = useRef<HTMLDivElement | null>(null);
   const about = useRef<HTMLDivElement | null>(null);
   const contact = useRef<HTMLDivElement | null>(null);
+
+  // Capture the window size
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Make sure to position the current page at the correct position based on the window size
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    switch (pathname) {
+      case "/":
+        (home.current as HTMLDivElement).scrollIntoView({
+          behavior: "smooth",
+        });
+        navigate("/");
+        break;
+      case "/blog":
+        (blog.current as HTMLDivElement).scrollIntoView({
+          behavior: "smooth",
+        });
+        navigate("/blog");
+        break;
+      case "/about":
+        (about.current as HTMLDivElement).scrollIntoView({
+          behavior: "smooth",
+        });
+        navigate("/about");
+        break;
+      case "/contact":
+        (contact.current as HTMLDivElement).scrollIntoView({
+          behavior: "smooth",
+        });
+        navigate("/contact");
+        break;
+      default:
+        break;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [windowSize]);
 
   const navItems = [
     {

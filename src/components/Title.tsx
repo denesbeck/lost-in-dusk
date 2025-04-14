8c0d3e9f14a7ad2f3c8a923e898d8028f4feb6cf
denesbeck <denesbeck@denesbeck-MacBook-Pro.local>
2025-04-14 07:43:26 +0200

    style: navbar logo

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,3 +1,4 @@
+import lostInDuskLogo from "../../public/lost_in_dusk_logo.png";
 interface TitleProps {
   mr?: boolean;
   ml?: boolean;
@@ -6,9 +7,14 @@ interface TitleProps {
 const Title = ({ mr = false, ml = false }: TitleProps) => {
   return (
     <div
-      className={`${mr && "mr-auto"} ${ml && "ml-auto"} animate-text-focus z-10 py-2 text-lg font-semibold select-none`}
+      className={`${mr && "mr-auto"} ${ml && "ml-auto"} animate-text-focus z-10 py-2 text-lg font-semibold text-nowrap select-none`}
     >
-      LostInDusk
+      <img
+        src={lostInDuskLogo}
+        alt="Lost In Dusk Logo"
+        className="inline-block mr-2 w-12 h-12"
+      />
+      <span className="ml-2 text-white">LostInDusk</span>
     </div>
   );
 };
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -18,7 +18,7 @@ const About = () => {
   return (
     <div className="flex flex-col items-center h-screen lg:justify-center">
       {/* wrapper for adding animate-slide-in-from-bottom */}
-      <div className="flex flex-col py-4 px-2 w-max ring-2 ring-teal-400 transition-all duration-200 ease-in-out lg:py-8 lg:px-8 lg:mt-0 animate-slide-in-from-bottom mt-[80px] max-h-[calc(100vh-170px)] max-w-[90vw] backdrop-blur-md lg:hover:shadow-[10px_10px_#1f2937,20px_20px_black]">
+      <div className="flex flex-col py-4 px-2 w-max ring-2 ring-teal-400 transition-all duration-200 ease-in-out lg:py-8 lg:px-8 lg:mt-0 animate-slide-in-from-bottom mt-[90px] max-h-[calc(100vh-170px)] max-w-[90vw] backdrop-blur-md lg:hover:shadow-[10px_10px_#1f2937,20px_20px_black]">
         <div ref={ref} className="overflow-auto w-full h-full">
           {/* main container */}
           <div className="grid grid-cols-1 gap-8 px-4 lg:grid-cols-2">

2448ede45679e2c6de49d6ce8906ee35a4912d58
denesbeck <denesbeck@denesbeck-MacBook-Pro.local>
2025-05-30 15:14:56 +0200

    refactor: cf site key

--- src/components/Contact/components/Turnstile.tsx
+++ src/components/Contact/components/Turnstile.tsx
@@ -2,7 +2,7 @@ const Turnstile = () => {
   return (
     <div
       className="p-0 cf-turnstile"
-      data-sitekey="0x4AAAAAABeK-0w6zpgPQB3k"
+      data-sitekey="0x4AAAAAABfQOswuSiTvm6ip"
       data-size="flexible"
       data-callback="onTurnstileSuccess"
     />

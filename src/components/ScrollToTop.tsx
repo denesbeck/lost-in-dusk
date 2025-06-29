a1cc0759c192656bb3bd16b9c5805acc2d6cef3a
denesbeck <denesbeck@denesbeck-MacBook-Pro.local>
2025-06-29 13:03:19 +0200

    fix: scroll to top persists search params

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,8 +1,9 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const ScrollToTop = () => {
   const navigate = useNavigate();
+  const { search } = useLocation();
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
@@ -24,7 +25,7 @@ const ScrollToTop = () => {
       {visible && (
         <button
           className="fixed right-6 bottom-6 z-20 p-2 text-white rounded-md cursor-pointer bg-secondary animate-text-focus"
-          onClick={() => navigate("/")}
+          onClick={() => navigate({ pathname: "/", search })}
         >
           Scroll to top
         </button>

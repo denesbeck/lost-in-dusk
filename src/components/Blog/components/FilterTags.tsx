74638e6a4304a5500ab3065832db8dabd33609cc
denesbeck <denesbeck@email.com>
2025-10-05 13:34:33 +0200

    fix: filter tags

--- src/components/Blog/components/FilterTags.tsx
+++ src/components/Blog/components/FilterTags.tsx
@@ -1,6 +1,7 @@
-import { Badge, IconButton } from "@mui/material";
+import { Badge, IconButton, Tooltip } from "@mui/material";
 import { useState } from "react";
 import { IoSettingsSharp, IoCheckmarkSharp, IoClose } from "react-icons/io5";
+import { LiaBroomSolid } from "react-icons/lia";
 import BLOG_ENTRIES from "../config/data";
 import DarkLayout from "@/components/DarkLayout";
 import { useNavigate, useSearchParams } from "react-router-dom";
@@ -23,7 +24,7 @@ const FilterTags = () => {
     }
   };
 
-  const handleSave = () => {
+  const handleApply = () => {
     navigate({
       pathname: "/blog",
       search: `?${selection.map((tag) => `tag=${tag}`).join("&")}`,
@@ -33,6 +34,16 @@ const FilterTags = () => {
 
   const handleCancel = () => {
     setSelection(tags);
+    setIsOpen(false);
+  };
+
+  const handleClear = () => {
+    setSelection([]);
+    navigate({
+      pathname: "/blog",
+      search: "",
+    });
+
     setIsOpen(false);
   };
 
@@ -49,12 +60,21 @@ const FilterTags = () => {
         <DarkLayout>
           <>
             <div className="flex absolute top-2 right-2 gap-4">
-              <IconButton onClick={handleCancel}>
-                <IoClose className="text-3xl text-white" />
-              </IconButton>
-              <IconButton onClick={handleSave}>
-                <IoCheckmarkSharp className="text-3xl text-primary" />
-              </IconButton>
+              <Tooltip arrow placement="bottom" title="Clear">
+                <IconButton onClick={handleClear}>
+                  <LiaBroomSolid className="text-3xl text-white" />
+                </IconButton>
+              </Tooltip>
+              <Tooltip arrow placement="bottom" title="Close">
+                <IconButton onClick={handleCancel}>
+                  <IoClose className="text-3xl text-white" />
+                </IconButton>
+              </Tooltip>
+              <Tooltip arrow placement="bottom" title="Apply">
+                <IconButton onClick={handleApply}>
+                  <IoCheckmarkSharp className="text-3xl text-primary" />
+                </IconButton>
+              </Tooltip>
             </div>
 
             <div className="flex overflow-auto flex-wrap gap-8 justify-center items-center p-4 animate-text-focus max-h-[80dvh] max-w-[50rem]">
@@ -62,7 +82,7 @@ const FilterTags = () => {
                 <button
                   key={tag}
                   onClick={() => handleSelect(tag)}
-                  className={`text-2xl ${selection.includes(tag) ? "text-primary scale-110" : "text-gray-200"} cursor-pointer transition-all duration-200 ease-in-out hover:scale-110`}
+                  className={`text-2xl ${tags.includes(tag) ? "text-primary scale-110" : "text-gray-200"} cursor-pointer transition-all duration-200 ease-in-out hover:scale-110`}
                 >
                   #{tag}
                 </button>
